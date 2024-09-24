607f568d15f9dcd581afefa680298c0b9cdc47ed
TyrellHaywood <tyrelltman@gmail.com>
2024-09-24 19:38:31 -0400

    feat: added project page + components w/ full responsive layout

--- components/ProjectTab.tsx
+++ components/ProjectTab.tsx
@@ -1,9 +1,13 @@
 const ProjectTab = () => {
   return (
-    <button className="w-[90vw] max-w-[365px] h-25px flex items-center my-[10px]">
-      <h3 className="flex-shrink-0 p-font font-light text-xl">CUSEC 2025</h3>
-      <div className="flex-grow border-t border-black border-dotted mx-1"></div>
-      <h3 className="flex-shrink-0 p-font font-light text-xl">2024</h3>
+    <button className="w-full h-[25px] lg:h-[45px] flex items-center my-[10px]">
+      <h3 className="flex-shrink-0 p-font font-light text-xl lg:text-3xl">
+        CUSEC 2025
+      </h3>
+      <div className="flex-grow border-t lg:border-t-2 border-black border-dotted mx-1"></div>
+      <h3 className="flex-shrink-0 p-font font-light text-xl lg:text-3xl">
+        2024
+      </h3>
     </button>
   );
 };
