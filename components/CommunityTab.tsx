340a51f860a454135bf59d4aa3c7454a4bf87d5a
TyrellHaywood <tyrelltman@gmail.com>
2024-09-30 23:52:22 -0400

    feat: updated rest of components for api call

--- components/CommunityTab.tsx
+++ components/CommunityTab.tsx
@@ -1,12 +1,20 @@
-const CommunityTab = () => {
+interface Community {
+  id: number;
+  title: string;
+  image: string;
+  description: string;
+  year: number;
+}
+
+const CommunityTab = ({ community }: { community: Community }) => {
   return (
     <button className="w-full h-[25px] lg:h-[45px] flex items-center my-[10px]">
       <h3 className="flex-shrink-0 p-font font-light text-xl lg:text-3xl">
-        Synchronize
+        {community.title}
       </h3>
       <div className="flex-grow border-t lg:border-t-2 border-black border-dotted mx-1"></div>
       <h3 className="flex-shrink-0 p-font font-light text-xl lg:text-3xl">
-        2024
+      {community.year}
       </h3>
     </button>
   );
